1633f15ec681cc93213dea6f46cab8a5d9193b84
backtofayton <yucelolcay@gmail.com>
2022-02-28 16:01:40 +0200

    Remove comments

--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
 import './style.scss';
 
-// You can specify which plugins you need
-// import { Tooltip, Toast, Popover } from 'bootstrap';`
-
 const leaderBoardApi = document.querySelector('#leaderboard-api');
 
 let gameID = 0;
@@ -13,19 +10,16 @@ async function createGameId() {
   let response = await fetch(`${leaderBoardApi.action}games/`, {
     method: 'POST',
     headers: {
-      'Content-Type': 'application/json', // 'Content-Type': 'application/x-www-form-urlencoded',
+      'Content-Type': 'application/json',
     },
     body: JSON.stringify(data),
   });
-  response = await response.json(); // .then(reply => reply.json())
-  // .then(json =>
+  response = await response.json();
 
   const { result } = response;
   const indexColumn = result.indexOf(':');
   const indexAdded = result.indexOf('added');
-  console.log(result);
   gameID = result.substring(indexColumn + 2, indexAdded - 1);
-  console.log(gameID);
 }
 
 async function createGame() {
@@ -41,10 +35,9 @@ async function createGame() {
     body: JSON.stringify(data),
   });
   saveGame = await saveGame.json();
-  console.log(saveGame);
 }
 
-function populateScores(games) {
+const populateScores = (games) => {
   if (games) {
     const scoreList = document.querySelector('#score-list');
     scoreList.innerHTML = '';
@@ -59,23 +52,21 @@ function populateScores(games) {
 }
 
 const leaderBoardSubmit = document.querySelector('#leaderboard-submit');
-async function submitHandler(e) {
+const submitHandler = async (e) => {
   e.preventDefault();
 
   if (!gameID) {
     await createGameId();
     createGame();
   } else {
-    console.log('there is game id');
     createGame();
   }
 }
 leaderBoardSubmit.addEventListener('click', submitHandler);
 
-async function btnRefreshHandler() {
+const btnRefreshHandler = async () => {
   let games = await fetch(`${leaderBoardApi.action}games/${gameID}/scores/`);
   games = await games.json();
-  console.log(games);
   populateScores(games.result);
 }
 const btnRefresh = document.querySelector('#btn-refresh');
